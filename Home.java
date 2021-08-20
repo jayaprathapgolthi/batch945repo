7b6a0063687e37c057a0a4b81e74aa1cf75f0163
root <root@ip-10-0-0-78.ap-south-1.compute.internal>
2021-08-20 17:52:16 +0000

    part of 2021

---
+++ Home.java
@@ -0,0 +1,6 @@
+class Home{
+	public static void main (string []args)
+	{
+		system.out.println("hello from hello.java file");
+	}
+}
---
+++ login.java
@@ -0,0 +1,6 @@
+class login{
+	public static void main (string args[])
+	{
+		system.out.println("hello from login.java file");
+	}
+}
